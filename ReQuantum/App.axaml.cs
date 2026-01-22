20ef7d53417e0da6efcc7e1f8241773f845ddc1a
Shoming <shoming@zju.edu.cn>
2026-01-22 15:57:49 +0800

    fix: 修复 GeneratedViewLocator 的 IL2066 警告
    - 将 GeneratedViewLocator 的注册方式从泛型改为工厂方法
    - 解决源生成器创建的类在编译时类型无法静态确定的问题

--- ReQuantum/App.axaml.cs
+++ ReQuantum/App.axaml.cs
@@ -71,7 +71,8 @@ public partial class App : Application
 
         serviceCollection.AddLogging(loggingBuilder => loggingBuilder.ClearProviders().AddSerilog(dispose: true));
 
-        serviceCollection.AddSingleton<IDataTemplate, GeneratedViewLocator>();
+        // IL2066 警告：泛型参数类型无法静态确定，原因：GeneratedViewLocator 是源生成器创建的类，编译时无法确定其具体类型，改为使用工厂方法
+        serviceCollection.AddSingleton<IDataTemplate>(sp => new GeneratedViewLocator(sp));
         serviceCollection.AddSingleton<System.Net.Http.HttpClient>();
         serviceCollection.AutoAddGeneratedServices();
 
