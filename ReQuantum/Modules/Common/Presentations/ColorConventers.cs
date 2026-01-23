e05ccd8f3a7c20db5bd9a98fcdb4a6e072e37aac
SzFxixi <1205795673@qq.com>
2026-01-23 15:46:01 +0800

    feat: 完成了课程成绩信息的展示

--- ReQuantum/Modules/Common/Presentations/ColorConventers.cs
+++ ReQuantum/Modules/Common/Presentations/ColorConventers.cs
@@ -1,4 +1,4 @@
-﻿using Avalonia.Data.Converters;
+using Avalonia.Data.Converters;
 using Avalonia.Media;
 
 namespace ReQuantum.Infrastructure.Presentations;
--- ReQuantum/Modules/Grade/Presentations/GradeViewModel.cs
+++ ReQuantum/Modules/Grade/Presentations/GradeViewModel.cs
@@ -59,5 +59,4 @@ public partial class GradeViewModel : ViewModelBase<GradeView>, IMenuItemProvide
     }
 
 
-
 }
